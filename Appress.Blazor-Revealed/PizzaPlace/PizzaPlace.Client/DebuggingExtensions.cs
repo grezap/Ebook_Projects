44801137698275db47b558062437545d0ea73996
gzapas <gzapas@yahoo.gr>
2019-05-17 18:52:06 +0300

    Added test Blazor apps

---
+++ Appress.Blazor-Revealed/PizzaPlace/PizzaPlace.Client/DebuggingExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Client
+{
+    public static class DebuggingExtensions
+    {
+        public static string ToJson(this object obj) => Microsoft.JSInterop.Json.Serialize(obj);
+    }
+}
